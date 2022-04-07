1a81db1b223c14352a4d2a1a00ed6bd8054f8ef2
Роксана Залесская <ksroksana@bk.ru>
2022-04-07 11:36:25 +0300

    Created Remove & Add projects

---
+++ PortfolioWebApp/Project.cs
@@ -0,0 +1,9 @@
+﻿namespace PortfolioWebApp
+{
+    public class Project
+    {
+        public string Name { get; set; }
+        public string Link { get; set; }
+        public string Description { get; set; }        
+    }
+}
---
+++ PortfolioWebApp/ProjectStorage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PortfolioWebApp
+{
+    public static class ProjectStorage
+    {
+        public static List<Project> Projects { get; private set; } = new List<Project>()
+        {
+            new Project
+            {
+                Name = "BullsAndCowsWindowsFormsApp",
+                Link = "https://github.com/Roksikod/BullsAndCowsWindowsFormsApp",
+                Description = "Windows Forms App was used to create this game",
+            }
+        };
+
+        public static void Add(Project project)
+        {
+            Projects.Add(project);
+        }
+
+        public static void RemoveByName(string name)
+        {
+            Projects.RemoveAll(x => x.Name == name);
+        }
+    }
+}
